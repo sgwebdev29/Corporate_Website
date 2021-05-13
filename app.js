03b9c1f47d3ff86351870d4f2304501d3278d7f9
sgwebdev29 <66306941+sgwebdev29@users.noreply.github.com>
2021-05-14 00:58:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var navLinks = document.querySelector('.nav-links')
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-50%";
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+I am not providing the images here in this project but you can easily download them from
+unsplash and pexels 
